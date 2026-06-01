e7675a3f02594073dfc96c355ee73dce37a3f007
sreenithadamayapally83 <sreenithadamayapally@gmail.com>
2026-06-01 16:06:34 +0530

    Bulk sync: 15 submissions

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& numbers,int target){
+        int l=0,r=numbers.size()-1;
+        while(l<r){
+            int currSum = numbers[l]+numbers[r];
+            if(currSum>target){
+            r--;
+            }
+            else if(currSum<target){
+            l++;
+            }
+            else{
+            return {l+1,r+1};
+            }
+        }
+        return {};
+    }
+};
