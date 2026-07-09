6b466fad18d34561341ed9130639f94cf5410e34
Sreenitha Damayapally <sreenithadamayapally@gmail.com>
2026-07-09 06:22:06 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution{
+public:
+    int maxProfit(vector<int>& prices){
+        int minBuy = prices[0];
+        int maxP =0;
+        for(int& sell:prices){
+            maxP = max(maxP,sell-minBuy);
+            minBuy = min(minBuy,sell);
+        }
+        return maxP;
+    }
+};
